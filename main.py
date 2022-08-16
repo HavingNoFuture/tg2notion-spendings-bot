3a97bde1a08aea35c6758a06709f068886f62fcb
Alexey Ermilov <alex.erm@yandex.ru>
2022-08-16 23:00:57 +0300

    test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(config.TELEGRAM_BOT_TOKEN)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, f'{message.text}, зайка!')
+    bot.send_message(message.chat.id, f'{message.text}, милашка!')
 
 
 if __name__ == '__main__':
